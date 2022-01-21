3e32a0633cdaee75bc16baf7747bba2ce937c0e4
Evgenii-Terekhov <mgpk1984@mail.ru>
2022-01-21 22:28:33 +0300

    done main and optional tasks

--- Input_output/Main_Task/src/main/java/TreeOfFiles.java
+++ Input_output/Main_Task/src/main/java/TreeOfFiles.java
@@ -6,6 +6,9 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
+//C:\\D\\Books\\empty_package\\file.txt
+//C:\\D\\Books
+
 public class TreeOfFiles {
 
     static int points = 0;
